fa96d77d864214b72cb1204cfbf7fa92aeef2a59
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-15 19:08:30 +0530

    init

--- hooks/useSocketConnection.ts
+++ hooks/useSocketConnection.ts
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
 import { socket } from "@/lib/socket";
+import { useSession } from "@/components/context/SessionContext";
 
 export function useSocketConnection() {
+  const { setState } = useSession();
 
   useEffect(() => {
     socket.connect();
 
-    socket.on("connect", () => {console.log("Connected to socket:" , socket.id);
-    })
+    socket.on("connect", () => setState("active"));
+    socket.on("disconnect", () => setState("disconnected"));
 
     return () => {
       socket.disconnect();
     };
- });
+  }, []);
 }
-
