0421bfa14b886e1541145aedff107aac28f36ba2
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-16 18:44:08 +0530

    init

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,14 @@
 export const SOCKET_EVENTS = {
+  CONNECT: "connect",
   JOIN_SESSION: "join-session",
-  CODE_CHANGE: "code-change",
-  CHAT_MESSAGE: "chat-message",
+  SEND_MESSAGE: "send-message",
+  RECEIVE_MESSAGE: "receive-message",
+  CODE_UPDATE: "code-update",
+  CURSOR_UPDATE: "cursor-update",
+  WEBRTC_OFFER: "webrtc-offer",
+  WEBRTC_ANSWER: "webrtc-answer",
+  ICE_CANDIDATE: "ice-candidate",
 };
-
 export const ROLES = {
   MENTOR: "mentor",
   STUDENT: "student",
