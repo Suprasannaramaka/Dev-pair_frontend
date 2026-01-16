64d34e5f13edf7bbdc87aa55352911f07b44f25f
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-16 18:33:16 +0530

    i

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+export const metaData =
+{
+    title: "DevPair – Mentor Student Platform",
+  description: "1-on-1 mentorship with live code collaboration",
+};
 export default function RootLayout({
   children,
 }: {
