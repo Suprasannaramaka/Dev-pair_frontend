772c5949c48bb3ee5309e8e04b8363f7a3749e98
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-17 17:52:51 +0530

    init

--- components/editor/CursorOverlay.tsx
+++ components/editor/CursorOverlay.tsx
@@ -15,12 +15,7 @@ export default function CursorOverlay({ cursors }: Props) {
   return (
     <div className="absolute inset-0 pointer-events-none">
       { cursors.map((cursor) => (
-        <div key={cursor.userId} className="absolute text-xs bg-blue-600 text-white px-1 rounded"
-          style= {{
-            top: cursor.line * 18,
-            left: cursor.column * 8,
-          }}
-        >
+        <div key={cursor.userId} className="absolute text-xs bg-blue-600 text-white px-1 rounded" >
           {cursor.username}
         </div>
       ))}
